82746c8571b6e03a9b12e52ccec742ee1e5aab75
Peter Bryant <pb@ptrbrynt.com>
2026-06-04 11:06:15 +0100

    Add hover and pressed states to ListItem

--- lib/src/components/molecules/list_item.dart
+++ lib/src/components/molecules/list_item.dart
@@ -1,11 +1,11 @@
+import 'package:catalyst_ui/src/tokens/tokens.dart';
 import 'package:flutter/widgets.dart';
 
 import '../../theme/extensions.dart';
-import '../../tokens/spacing.dart';
 
 /// A single row in a list with optional leading icon, title, subtitle,
 /// and trailing widget.
-class ListItem extends StatelessWidget {
+class ListItem extends StatefulWidget {
   /// Creates a list item.
   const ListItem({
     required this.title,
@@ -40,6 +40,16 @@ class ListItem extends StatelessWidget {
   final EdgeInsets padding;
 
   @override
+  State<ListItem> createState() => _ListItemState();
+}
+
+class _ListItemState extends State<ListItem> {
+  bool get _interactive => widget.onTap != null;
+
+  bool _hovered = false;
+  bool _pressed = false;
+
+  @override
   Widget build(BuildContext context) {
     final cs = context.colorScheme;
     final motion = context.motion;
@@ -47,64 +57,107 @@ class ListItem extends StatelessWidget {
     return AnimatedContainer(
       duration: motion.standard.duration,
       curve: motion.standard.curve,
-      padding: padding,
+      padding: widget.padding,
       decoration: BoxDecoration(
         border:
-            divider ? Border(bottom: BorderSide(color: cs.borderSubtle)) : null,
+            widget.divider
+                ? Border(bottom: BorderSide(color: cs.borderSubtle))
+                : null,
       ),
       child: MouseRegion(
-        cursor: onTap != null ? SystemMouseCursors.click : MouseCursor.defer,
+        cursor: _interactive ? SystemMouseCursors.click : MouseCursor.defer,
+        onEnter:
+            _interactive
+                ? (_) {
+                  setState(() {
+                    _hovered = true;
+                  });
+                }
+                : null,
+        onExit:
+            _interactive
+                ? (_) {
+                  setState(() {
+                    _pressed = false;
+                    _hovered = false;
+                  });
+                }
+                : null,
         child: GestureDetector(
-          onTap: onTap,
-          child: Row(
-            spacing: Spacing.s3,
-            children: [
-              if (leading != null)
-                IconTheme(
-                  data: IconThemeData(color: cs.brand),
-                  child: leading!,
-                ),
-              Expanded(
-                child: AnimatedSize(
-                  duration: motion.standard.duration,
-                  curve: motion.standard.curve,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisSize: MainAxisSize.min,
-                    spacing: 2,
-                    children: [
-                      DefaultTextStyle(
-                        style: TextStyle(
-                          fontFamily: context.typography.fontFamily,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 15,
-                          height: 1.3,
-                          color: cs.text,
-                        ),
-                        child: title,
-                      ),
-                      if (subtitle != null)
+          onTap: widget.onTap,
+          onTapDown:
+              _interactive
+                  ? (_) {
+                    setState(() {
+                      _pressed = true;
+                    });
+                  }
+                  : null,
+          onTapUp:
+              _interactive
+                  ? (_) {
+                    setState(() {
+                      _pressed = false;
+                    });
+                  }
+                  : null,
+          child: AnimatedContainer(
+            duration: motion.micro.duration,
+            curve: motion.micro.curve,
+            decoration: BoxDecoration(
+              borderRadius: Radii.smAll,
+              color: _hovered ? context.colorScheme.muted : null,
+            ),
+            child: Row(
+              spacing: Spacing.s3,
+              children: [
+                if (widget.leading != null)
+                  IconTheme(
+                    data: IconThemeData(color: cs.brand),
+                    child: widget.leading!,
+                  ),
+                Expanded(
+                  child: AnimatedSize(
+                    duration: motion.standard.duration,
+                    curve: motion.standard.curve,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisSize: MainAxisSize.min,
+                      spacing: 2,
+                      children: [
                         DefaultTextStyle(
                           style: TextStyle(
                             fontFamily: context.typography.fontFamily,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 13,
-                            height: 1.4,
-                            color: cs.textMuted,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 15,
+                            height: 1.3,
+                            color: cs.text,
                           ),
-                          child: subtitle!,
+                          child: widget.title,
                         ),
-                    ],
+                        if (widget.subtitle != null)
+                          DefaultTextStyle(
+                            style: TextStyle(
+                              fontFamily: context.typography.fontFamily,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 13,
+                              height: 1.4,
+                              color: cs.textMuted,
+                            ),
+                            child: widget.subtitle!,
+                          ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              if (trailing != null)
-                IconTheme(
-                  data: IconThemeData(color: cs.textMuted, size: 16),
-                  child: trailing!,
-                ),
-            ],
-          ),
+                if (widget.trailing != null)
+                  IconTheme(
+                    data: IconThemeData(color: cs.textMuted, size: 16),
+                    child: widget.trailing!,
+                  ),
+              ],
+            ),
+          ).withBrightness(_pressed ? 0.92 : 1),
         ),
       ),
     );
