05869f196b831595e69d2b013018270c8c004d71
Peter Bryant <pb@ptrbrynt.com>
2026-06-08 11:53:30 +0100

    Bug fix

--- lib/src/components/molecules/menu_button.dart
+++ lib/src/components/molecules/menu_button.dart
@@ -366,6 +366,7 @@ class _MenuOptionRowState extends State<_MenuOptionRow> {
                   widget.option.label,
                   style: widget.typography.p3.copyWith(
                     color: widget.colorScheme.text,
+                    height: 1,
                   ),
                 ),
               ),
